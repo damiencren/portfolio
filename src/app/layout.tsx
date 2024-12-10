3f41f94cc71d20c48400b6987e74353451b19a32
Damien Cren <68796664+damiencren@users.noreply.github.com>
2024-12-10 02:28:29 -0500

    add metadatas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,20 +9,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <head>
         <title>Damien CREN</title>
         <link rel="/favicon.ico" href="/DC.ico"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
-        <meta name="description" content="Portfolio présentant les projets de développement web et les compétences de Damien CREN, développeur freelance full-stack." />
-        <meta name="keywords" content="Développement, web, freelance, full-stack, Damien, CREN, portfolio" />
+        <meta name="description" content="Portfolio présentant mes projets, mes compétences ainsi que mon experience en développement web full-stack. Damien CREN" />
+        <meta name="keywords" content="Développement, web, freelance, full-stack, Damien, CREN, portfolio, damiencren, Laval, Rennes, Developpeur, informatique" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="author" content="Damien CREN" />
         <meta name="robots" content="index, follow" />
-        {/* <meta property="og:title" content="Titre de votre page pour les réseaux sociaux" />
-        <meta property="og:description" content="Description de votre page pour les réseaux sociaux" />
-        <meta property="og:image" content="URL de l'image à afficher sur les réseaux sociaux" />
-        <meta property="og:url" content="URL de votre page" /> */}
+        <meta property="og:title" content="Damien CREN - Portfolio" />
+        <meta property="og:description" content="Découvrez mes projets, compétences et expérience en développement web full-stack." />
+        <meta property="og:url" content="damiencren.me" />
+        <meta property="og:image" content="/me.jpg" />
+        <meta property="og:type" content="website" />
+        <meta http-equiv="Content-Language" content="fr"/>
+        <link rel="canonical" href="https://damiencren.me" />
       </head>
       <body>
         <ThemeProvider
