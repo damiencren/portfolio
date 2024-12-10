210e9b40e179dba90460897dd5c704d33fbdd3eb
Damien Cren <68796664+damiencren@users.noreply.github.com>
2024-12-10 02:20:14 -0500

    add data to resume.ts

--- src/app/data/resume.ts
+++ src/app/data/resume.ts
@@ -1,3 +1,4 @@
+import { url } from "inspector";
 import { title } from "process";
 
 export const DATA = {
@@ -5,7 +6,7 @@ export const DATA = {
   location: "Saguenay CA",
   description:
     "Developpeur full stack en étude à Chicoutimi, Quebec",
-  aboutText:"Je développe mes compétences en développement logiciel avec des technologies comme ***[TypeScript](https://www.typescriptlang.org/)***, ***[Next.js](https://nextjs.org/)*** et ***[Tailwind](https://tailwindcss.com/)*** pour le front-end. Côté back-end, je me spécialise en ***[Java](https://www.java.com/fr/)*** et ***[Python](https://www.python.org/)***. J’utilise également ***[Git](https://git-scm.com/)*** pour la gestion des versions et ***[SQL](https://en.wikipedia.org/wiki/SQL)*** pour les bases de données. En parallèle, je m'intéresse beaucoup à l'intelligence artificielle.",
+  aboutText: "Je développe mes compétences en développement logiciel avec des technologies comme ***[TypeScript](https://www.typescriptlang.org/)***, ***[Next.js](https://nextjs.org/)*** et ***[Tailwind](https://tailwindcss.com/)*** pour le front-end. Côté back-end, je me spécialise en ***[Java](https://www.java.com/fr/)*** et ***[Python](https://www.python.org/)***. J’utilise également ***[Git](https://git-scm.com/)*** pour la gestion des versions et ***[SQL](https://en.wikipedia.org/wiki/SQL)*** pour les bases de données. En parallèle, je m'intéresse beaucoup à l'intelligence artificielle.",
   avatarUrl: "/me.jpg",
 
   socials: [
@@ -23,7 +24,7 @@ export const DATA = {
     },
     {
       label: "CV",
-      url: "https://www.instagram.com/damien-photos/",
+      url: "/CV.pdf",
       icon: "Download",
       variant: "secondary",
     }
@@ -47,7 +48,7 @@ export const DATA = {
     {
       school: "Université du Quebec à Chicoutimi (UQAC)",
       href: "https://www.uqac.ca/",
-      degree: "Baccalaureat Informatique Double-Diplome",
+      degree: "Baccalaureat en Informatique (Double-Diplome)",
       logoUrl: "/logos/uqac.png",
       start: "2024",
       end: "2024",
@@ -55,7 +56,7 @@ export const DATA = {
     {
       school: "IUT de Laval",
       href: "https://iut-laval.univ-lemans.fr/fr/index.html",
-      degree: "DUT Informatique - Parcorus Réalisation d'applications",
+      degree: "BUT Informatique - Parcorus Réalisation d'applications",
       logoUrl: "/logos/iut_laval.jpg",
       start: "2022",
       end: "2024",
@@ -63,52 +64,66 @@ export const DATA = {
   ],
   projects: [
     {
-      title: "This portfolio",
-      href: "damiencren.me",
+      title: "Portfolio",
+      url: "damiencren.me",
+      href: "https://damiencren.me",
       dates: "Sept 2024 - Oct 2024",
       active: true,
       description:
         "J'ai réalisé ce portfolio afin de me présenter et de montrer mes compétences en développement web. J'y partage mes projets, mon parcours et mes compétences.",
-      repository:"https://github.com/damiencren/portfolio",
+      repository: "https://github.com/damiencren/portfolio",
       image: "/screens/proj_2.png",
-      technos : ["Next.js", "TypeScript", "Tailwind CSS"],
+      technos: ["Next.js", "TypeScript", "Tailwind CSS"],
     },
     {
-      title: "Spotify Clone",
+      title: "Clone Spotify",
+      url: "",
       href: "",
       dates: "Dec 2024 - Nov 2024",
       active: false,
       description:
         "Ce site web est un clone du client spotify qui utilise l'API et l'APK de Spotify. J'ai développé cette application dans le cadre d'un projet universitaire.",
-      repository:"https://github.com/damiencren/music_player",
+      repository: "https://github.com/damiencren/music_player",
       image: "/screens/proj_3.png",
-      technos : ["Vue.js", "JavaScript", "Tailwind CSS"],
+      technos: ["Vue.js", "JavaScript", "Tailwind CSS"],
     },
     {
       title: "Mace Renovation",
+      url: "mace-renovation.fr",
       href: "http://57.129.40.117/",
       dates: "Sept 2024 - Oct 2024",
       active: true,
       description:
         "Site vitrine réalisé pour un client spécialisé dans la rénovation. ",
-      repository:"https://github.com/damiencren/mace_website",
+      repository: "https://github.com/damiencren/mace_website",
       image: "/screens/proj_1.png",
-      technos : ["Next.js", "TypeScript", "Tailwind CSS"],
+      technos: ["Next.js", "TypeScript", "Tailwind CSS"],
     }
   ],
   hackathons: [
+    {
+      title: "Nuit de l'Info",
+      dates: "Dec 2024",
+      location: "Laval, FR",
+      description:
+        "Développement en une nuit d'un site web visant à sensibiliser sur les écosystèmes marins, en établissant un parallèle entre le corps humain et les océans.",
+      image:
+        "/logos/24h_info_24.png",
+      links: [
+      ],
+    },
     {
       title: "24H de l'Info",
       dates: "May 2023",
       location: "Bourg-en-Bresse, FR",
       description:
-      "Hackathon national en équipe de 4 durant lequel j'ai pu représenter mon IUT. Création de site web, résolution de problèmes algorithmiques, OSINT",
+        "Hackathon national en équipe de 4 durant lequel j'ai pu représenter mon IUT. Création de site web, résolution de problèmes algorithmiques, OSINT",
       image:
         "/logos/24h_info.png",
       links: [
         {
-          href:"https://www.leprogres.fr/education/2023/05/14/24-h-info-la-competition-qui-marquera-l-iut-informatique",
-          title:"Article sur l'événement",
+          href: "https://www.leprogres.fr/education/2023/05/14/24-h-info-la-competition-qui-marquera-l-iut-informatique",
+          title: "Article sur l'événement",
         }
       ],
     },
@@ -117,13 +132,13 @@ export const DATA = {
       dates: "Dec 2022",
       location: "Laval, FR",
       description:
-      "Développement en équipe d'une application qui sensibilise sur les MST, IST et la santé sexuelle. ",
+        "Développement en équipe d'une application qui sensibilise sur les MST, les IST et la santé sexuelle. ",
       image:
         "/logos/nuit_info.png",
       links: [
         {
-          href:"https://www.ouest-france.fr/pays-de-la-loire/laval-53000/a-laval-une-nuit-pour-developper-une-application-d-information-sur-le-sida-et-la-sante-sexuelle-c6eeba0e-7194-11ed-8d7c-fc31769d4d38",
-          title:"Article avec notre Equipe Iclipse en Couverture",
+          href: "https://www.ouest-france.fr/pays-de-la-loire/laval-53000/a-laval-une-nuit-pour-developper-une-application-d-information-sur-le-sida-et-la-sante-sexuelle-c6eeba0e-7194-11ed-8d7c-fc31769d4d38",
+          title: "Article avec notre Equipe Iclipse en Couverture",
         }
       ],
     },
